4707584b694b1865ac84cf06c735102cc5c0cf90
hy-reza <hy.rezaalfanda@gmail.com>
2023-02-07 14:16:44 +0700

    Learn Function Usage & Types

---
+++ function.ts
@@ -0,0 +1,13 @@
+function sum(n1: number, n2: number): number {
+  //type dari return value
+  return n1 + n2;
+}
+
+function display(text: string, cb: (n1: number, n2: number) => number): void {
+  console.info(text, cb(1, 2));
+}
+
+let result: (i1: number, i2: number) => number; //type function
+result = sum;
+
+display("Result : ", result);
